6932c9d35432a83aa1e5b2dd63661632d41ba97d
Gerark <st4lk3r87@gmail.com>
2024-05-13 01:24:18 +0200

    Add .editorconfig to the src root folder

--- src/Apps/ProjectTactics/ProjectTacticsMain.cpp
+++ src/Apps/ProjectTactics/ProjectTacticsMain.cpp
@@ -1,4 +1,9 @@
-int main(int argc, char *argv)
-{
-    return 0;
+#include <iostream>
+
+void justAFunctionToCheckCompilation() {
+	std::cout << "HELLO WORLD!!!" << std::endl;
+}
+
+int main(int argc, char* argv) {
+	justAFunctionToCheckCompilation();
 }
