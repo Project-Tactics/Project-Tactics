4a274399e669ef5e534e323231df1b3b1ee8232b
Gerark <st4lk3r87@gmail.com>
2024-05-14 23:03:09 +0200

    Add glm as thirdparty deps

--- src/Engine/Application/States/MapState.cpp
+++ src/Engine/Application/States/MapState.cpp
@@ -1,5 +1,12 @@
 #include "MapState.h"
 
+#include <glm/vec3.hpp> // glm::vec3
+#include <glm/vec4.hpp> // glm::vec4
+#include <glm/mat4x4.hpp> // glm::mat4
+#include <glm/ext/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale
+#include <glm/ext/matrix_clip_space.hpp> // glm::perspective
+#include <glm/ext/scalar_constants.hpp> // glm::pi
+
 namespace tactics {
 
 FsmAction MapState::enter() {
@@ -14,6 +21,14 @@ FsmAction MapState::update() {
 	if (_exitNextFrame) {
 		return FsmAction::transition("exit");
 	}
+
+	float translate = 3;
+	glm::vec2 rotate = {};
+	glm::mat4 Projection = glm::perspective(glm::pi<float>() * 0.25f, 4.0f / 3.0f, 0.1f, 100.f);
+	glm::mat4 View = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -translate));
+	View = glm::rotate(View, rotate.y, glm::vec3(-1.0f, 0.0f, 0.0f));
+	View = glm::rotate(View, rotate.x, glm::vec3(0.0f, 1.0f, 0.0f));
+	glm::mat4 Model = glm::scale(glm::mat4(1.0f), glm::vec3(0.5f));
 
 	return FsmAction::none();
 }
