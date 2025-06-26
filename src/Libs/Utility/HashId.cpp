c3e315749346fa8ae10e7c169818d7be0b9703df
Gerark <st4lk3r87@gmail.com>
2025-06-26 23:24:11 +0200

    Compile fix on release builds

--- src/Libs/Utility/HashId.cpp
+++ src/Libs/Utility/HashId.cpp
@@ -31,7 +31,9 @@ HashId::HashId(const char* str) {
 	}
 	map[hash] = str;
 	_id = hash;
-	_debugName = str;
+#ifdef _DEBUG
+	_debugName = map[hash];
+#endif
 }
 
 HashId::HashId(const std::string& str) : HashId(str.c_str()) {}
